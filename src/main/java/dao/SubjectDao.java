989cd090b9ebf6cf8f8c2df25190a03ac46e99c2
t_anhnie <t_anhnie@10.202.17.63>
2026-04-23 10:24:40 +0900

    nie 4月23日追加

--- src/main/java/dao/SubjectDao.java
+++ src/main/java/dao/SubjectDao.java
@@ -1,9 +1,54 @@
 package dao;
 
-public class SubjectDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-	public SubjectDao() {
-		// TODO 自動生成されたコンストラクター・スタブ
-	}
+import bean.School;
+import bean.Subject;
 
+public class SubjectDao extends Dao {
+
+    /**
+     * Lấy danh sách môn học theo trường (Dùng cho trang List)
+     */
+    public List<Subject> filter(School school) throws Exception {
+        List<Subject> list = new ArrayList<>();
+        Connection con = getConnection();
+        PreparedStatement st = con.prepareStatement(
+            "SELECT * FROM SUBJECT WHERE SCHOOL_CD = ? ORDER BY CD ASC"
+        );
+        st.setString(1, school.getCd());
+        ResultSet rs = st.executeQuery();
+
+        while (rs.next()) {
+            Subject subject = new Subject();
+            subject.setCd(rs.getString("CD"));
+            subject.setName(rs.getString("NAME"));
+            list.add(subject);
+        }
+        st.close();
+        con.close();
+        return list;
+    }
+
+    /**
+     * Thêm môn học mới (Dùng cho trang Create)
+     */
+    public boolean create(Subject subject) throws Exception {
+        Connection con = getConnection();
+        PreparedStatement st = con.prepareStatement(
+            "INSERT INTO SUBJECT (SCHOOL_CD, CD, NAME) VALUES (?, ?, ?)"
+        );
+        st.setString(1, subject.getSchool().getCd());
+        st.setString(2, subject.getCd());
+        st.setString(3, subject.getName());
+
+        int count = st.executeUpdate();
+        st.close();
+        con.close();
+        return count > 0;
+    }
 }
